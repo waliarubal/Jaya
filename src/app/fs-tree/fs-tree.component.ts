cbcfa5c1b7afcfe35eb80f093c68c3e4aacb480f
Rubal Walia <walia.rubal@gmail.com>
2019-04-19 20:12:05 +0530

    Staging

--- src/app/fs-tree/fs-tree.component.ts
+++ src/app/fs-tree/fs-tree.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { BaseComponent } from '../base.component';
 import { MessageModel } from '@shared/models/message.model';
+import { ElectronService } from 'ngx-electron';
 
 @Component({
     selector: 'app-fs-tree',
     templateUrl: './fs-tree.component.html'
 })
 export class FileSystemTreeComponent extends BaseComponent {
+
+    constructor(electron: ElectronService) {
+        super(electron);
+    }
 
     protected OnMessage(message: MessageModel): void {
         console.log(message);
