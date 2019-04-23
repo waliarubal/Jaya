85cd8223588b8a34958849d4c9b51eb2ea02c96e
Rubal Walia <walia.rubal@gmail.com>
2019-04-23 19:16:50 +0530

    Staging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IpcService } from '@services/ipc.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [IpcService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
