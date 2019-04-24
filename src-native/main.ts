72788103b17b200c5fecf862d04353c2b81ae873
Rubal Walia <walia.rubal@gmail.com>
2019-04-24 15:09:13 +0530

    Created main class.

--- src-native/main.ts
+++ src-native/main.ts
@@ -4,58 +4,68 @@ import * as Url from 'url';
 import { IpcService, FileSystemService } from './services';
 import { BaseService } from '../src-common';
 
-let _window: BrowserWindow;
-let _services: BaseService[];
+class Main {
+    private _window: BrowserWindow;
+    private _services: BaseService[];
 
-function CreateWindow(): void {
-    if (_window)
-        return;
-
-    const path = Path.join(__dirname, '../../dist/jaya/index.html');
-    const url = Url.format({
-        pathname: path,
-        protocol: 'file:',
-        slashes: true
-    });
-
-    _window = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    });
-
-    _services = CreateServices(_window);
-
-    _window.loadURL(url);
-    _window.webContents.openDevTools();
-    _window.on('closed', DestroyWindow);
-}
-
-function DestroyWindow(): void {
-    if (_window) {
-        _window = null;
+    Initialize(): void {
+        app.on('ready',() => this.CreateWindow());
+        app.on('activate', () => this.CreateWindow());
+        app.on('window-all-closed', () => this.DestroyWindow());
     }
 
-    if (process.platform !== 'darwin')
-        app.quit();
+    private CreateServices(window: BrowserWindow): BaseService[] {
+        let ipc = new IpcService(window);
+        let services: BaseService[] = [
+            ipc,
+            new FileSystemService(ipc)
+        ];
+        return services;
+    }
+
+    private DestroyServices(services: BaseService[]): void {
+
+    }
+
+    private CreateWindow(): void {
+        if (this._window)
+            return;
+
+        const path = Path.join(__dirname, '../../dist/jaya/index.html');
+        const url = Url.format({
+            pathname: path,
+            protocol: 'file:',
+            slashes: true
+        });
+
+        let window = this._window = new BrowserWindow({
+            width: 800,
+            height: 600,
+            webPreferences: {
+                nodeIntegration: true,
+                contextIsolation: false
+            }
+        });
+
+        this._services = this.CreateServices(window);
+
+        window.loadURL(url);
+        window.webContents.openDevTools();
+        window.on('closed', () => this.DestroyWindow());
+    }
+
+    private DestroyWindow(): void {
+        if (this._services)
+            this.DestroyServices(this._services);
+
+        if (this._window)
+            this._window = null;
+
+        if (process.platform !== 'darwin')
+            app.quit();
+    }
+
 }
 
-function CreateServices(window: BrowserWindow): BaseService[] {
-    let ipc = new IpcService(window);
-    let services: BaseService[] = [
-        ipc,
-        new FileSystemService(ipc)
-    ];
-    return services;
-}
-
-function DestroyServices(services: BaseService[]): void {
-
-}
-
-app.on('ready', CreateWindow);
-app.on('activate', CreateWindow);
-app.on('window-all-closed', DestroyWindow);
+let _main = new Main();
+_main.Initialize();
