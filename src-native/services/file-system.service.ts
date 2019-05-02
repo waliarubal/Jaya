129da5d2d38c57b17cce7adbafa2618776c338c2
Rubal Walia <walia.rubal@gmail.com>
2019-05-03 00:55:55 +0530

    Added code to get drive letters in win32.

--- src-native/services/file-system.service.ts
+++ src-native/services/file-system.service.ts
@@ -1,5 +1,6 @@
 import * as Fs from 'fs';
 import * as Os from 'os';
+import { spawn } from 'child_process';
 import * as Path from 'path';
 import { IpcService } from './ipc.service';
 import { BaseService, Constants, MessageModel, MessageType, DirectoryModel, Helpers, FileModel, ProviderModel } from '../../src-common';
@@ -15,6 +16,34 @@ export class FileSystemService extends BaseService {
 
     protected Dispose(): void {
         this._ipc.Receive.removeAllListeners(Constants.IPC_CHANNEL);
+    }
+
+    private async GetDriveLetters(): Promise<string[]> {
+        return new Promise<string[]>((resolve, reject) => {
+            let cmd = spawn('cmd');
+            cmd.stdout.on('data', (chunk: string | Buffer) => {
+                let output = String(chunk).split(/[\r\n]+/);
+                if (output.length > 0 && output[0].startsWith('Name')) {
+                    let letters: string[] = [];
+                    output.splice(1).forEach(s => {
+                        let letter = s.trim();
+                        if (letter.length === 2 && letter[1] === ':')
+                            letters.push(letter);
+                    });
+                    resolve(letters);
+                }
+            });
+            cmd.stderr.on('data', (chunk: string | Buffer) => {
+                let data = String(chunk);
+                reject(data);
+            });
+            cmd.on('exit', (code: number, signal: string) => {
+                if (code !== 0)
+                    reject(signal);
+            });
+            cmd.stdin.write('wmic logicaldisk get name\n');
+            cmd.stdin.end();
+        });
     }
 
     private async GetFileInfo(path: string): Promise<Fs.Stats> {
@@ -86,10 +115,21 @@ export class FileSystemService extends BaseService {
     private async GetProvider(): Promise<ProviderModel> {
         let provider = ProviderModel.New(`Computer (${Os.hostname()})`, 'fa fa-laptop');
         return new Promise<ProviderModel>(async (resolve, reject) => {
-            Os.hostname();
             try {
-                let directories = await this.GetDirectory(Os.homedir());
-                provider.Directories = directories.Directories;
+                let driveLetters = await this.GetDriveLetters();
+
+                let directories: DirectoryModel[] = [];
+                for (let letter of driveLetters) {
+                    let directory = new DirectoryModel();
+                    directory.Name = letter;
+                    directory.Path = Path.join(letter, '/');
+                    directories.push(directory);
+                }
+
+                provider.Directories = directories;
+
+                // let directories = await this.GetDirectory(Os.homedir());
+                // provider.Directories = directories.Directories;
             } catch (ex) {
                 console.log(ex);
             }
