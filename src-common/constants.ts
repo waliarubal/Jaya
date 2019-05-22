cf2e712ce2d679d40c4de43ce9952a2422f875ef
Rubal Walia <walia.rubal@gmail.com>
2019-05-22 19:21:37 +0530

    Added initial menu data.

--- src-common/constants.ts
+++ src-common/constants.ts
@@ -1,9 +1,8 @@
-export class Constants {
-    static readonly IPC_CHANNEL: string = 'ipc_channel';
-    static readonly SEPARATOR: string = '¶';
-}
-
 export enum Commands {
+    File,
+    Edit,
+    View,
+    Help,
     Cut = 'Cut',
     Copy = 'Copy',
     Paste = 'Paste',
@@ -29,4 +28,48 @@ export enum Commands {
     Update = 'Check for Updates...',
     About = 'About',
     PinToQuickAccess = 'Pin to Quick access'
+}
+
+export interface ICommand {
+    readonly Command?: Commands;
+    readonly Title?: string;
+    readonly IconClass?: string;
+    readonly IsSeparator?: boolean;
+    readonly Commands?: ICommand[];
+}
+
+export class Constants {
+    static readonly IPC_CHANNEL: string = 'ipc_channel';
+    static readonly SEPARATOR: string = '¶';
+
+    static readonly MENU_DATA: ICommand[] = [
+        {
+            Command: Commands.File,
+            Title: 'File',
+            Commands: [
+                {
+                    Command: Commands.NewFolder,
+                    Title: 'New folder',
+                    IconClass: 'fa fa-folder-plus',
+                },
+                { IsSeparator: true },
+                {
+                    Command: Commands.Properties,
+                    Title: 'Properties',
+                    IconClass: 'fa fa-info'
+                },
+                {
+                    Command: Commands.Open,
+                    Title: 'Open',
+                    IconClass: 'fa fa-edit'
+                },
+                { IsSeparator: true },
+                {
+                    Command: Commands.Exit,
+                    Title: 'Exit',
+                    IconClass: 'fa fa-sign-out-alt'
+                }
+            ]
+        },
+    ];
 }
