77224ed62169b72aa062c6bfe4ca7162990ff791
Rubal Walia <walia.rubal@gmail.com>
2019-08-12 15:13:40 +0530

    Added some UI hanges.

---
+++ Jaya.Ui/Views/DetailsView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Jaya.Ui.Views
+{
+    public class DetailsView : UserControl
+    {
+        public DetailsView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
