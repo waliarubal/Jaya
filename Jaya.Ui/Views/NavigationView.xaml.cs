67dd7cedde036e8bc219769443f99a5fe610d0ea
Rubal Walia <walia.rubal@gmail.com>
2019-08-12 12:22:16 +0530

    Added navigation view.

---
+++ Jaya.Ui/Views/NavigationView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Jaya.Ui.Views
+{
+    public class NavigationView : UserControl
+    {
+        public NavigationView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
