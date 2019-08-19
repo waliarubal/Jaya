f0971b0bee113d38bb3bc36dc7eff29b92015db0
Rubal Walia <walia.rubal@gmail.com>
2019-08-20 00:13:22 +0530

    Staging

---
+++ Jaya.Ui/ViewModels/AddressbarViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Jaya.Ui.ViewModels
+{
+    public class AddressbarViewModel : ViewModelBase
+    {
+        public string SearchQuery
+        {
+            get => Get<string>();
+            set
+            {
+                Set(value);
+                RaisePropertyChanged(nameof(IsSearchQueryEmpty));
+            }
+        }
+
+        public bool IsSearchQueryEmpty => string.IsNullOrEmpty(SearchQuery);
+
+        public void ClearSearchQuery()
+        {
+            SearchQuery = null;
+        }
+    }
+}
