e94ccea74f26bdfc85afb0233842d3d9b11991e0
Rubal Walia <walia.rubal@gmail.com>
2019-08-21 17:49:45 +0530

    Staging

---
+++ Jaya.Ui/Views/ExplorerView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Jaya.Ui.Views
+{
+    public class ExplorerView : UserControl
+    {
+        public ExplorerView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
