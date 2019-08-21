e4d24972e57595e6f641cdc8dd74beb814bc44b1
Rubal Walia <walia.rubal@gmail.com>
2019-08-21 18:10:39 +0530

    Staging

---
+++ Jaya.Ui/Views/ProvidersWindowView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Jaya.Ui.Views
+{
+    public class ProvidersWindowView : Window
+    {
+        public ProvidersWindowView()
+        {
+            this.InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
