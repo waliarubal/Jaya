3e22edfbd71bcb8e15f05a4cebc3afd4ccffbc79
Rubal Walia <walia.rubal@gmail.com>
2019-08-30 23:18:52 +0530

    Staging

--- Jaya.Ui/ViewModels/MainWindowViewModel.cs
+++ Jaya.Ui/ViewModels/MainWindowViewModel.cs
@@ -5,15 +5,39 @@ namespace Jaya.Ui.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        readonly Subscription<DirectoryChangedEventArgs> _onDirectoryChanged;
         readonly ConfigurationService _configService;
 
         public MainWindowViewModel()
         {
+            WindowTitle = "Jaya";
+            _onDirectoryChanged = EventAggregator.Subscribe<DirectoryChangedEventArgs>(DirectoryChanged);
             _configService = GetService<ConfigurationService>();
+        }
+
+        ~MainWindowViewModel()
+        {
+            EventAggregator.UnSubscribe(_onDirectoryChanged);
         }
 
         public ToolbarConfigModel ToolbarConfig => _configService.ToolbarConfiguration;
 
         public PaneConfigModel PaneConfig => _configService.PaneConfiguration;
+
+        public string WindowTitle
+        {
+            get => Get<string>();
+            private set => Set(value);
+        }
+
+        void DirectoryChanged(DirectoryChangedEventArgs args)
+        {
+            if (args.Provider == null)
+                WindowTitle = "Jaya";
+            else if (args.Directory == null || string.IsNullOrEmpty(args.Directory.Path))
+                WindowTitle = args.Provider.Name;
+            else
+                WindowTitle = args.Directory.Name;
+        }
     }
 }
