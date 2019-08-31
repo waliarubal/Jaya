540a194783c27480e95a296963e1ebeba3ed536e
Rubal Walia <walia.rubal@gmail.com>
2019-08-31 12:50:10 +0530

    Data loading performance optimizations.

--- Jaya.Ui/Converters/FileSystemObjectSizeToStringConverter.cs
+++ Jaya.Ui/Converters/FileSystemObjectSizeToStringConverter.cs
@@ -1,25 +1,13 @@
 ﻿using Avalonia.Data.Converters;
+using Jaya.Ui.Models;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 
 namespace Jaya.Ui.Converters
 {
-    // logic taken from https://stackoverflow.com/questions/14488796/does-net-provide-an-easy-way-convert-bytes-to-kb-mb-gb-etc
     public class FileSystemObjectSizeToStringConverter : IMultiValueConverter
     {
-        readonly string[] _sizeSuffixes =
-        {
-            "bytes",
-            "KB",
-            "MB",
-            "GB",
-            "TB",
-            "PB",
-            "EB",
-            "ZB",
-            "YB"
-        };
 
         public object Convert(IList<object> values, Type targetType, object parameter, CultureInfo culture)
         {
@@ -28,37 +16,7 @@ namespace Jaya.Ui.Converters
                 return string.Empty;
 
             var sizeInBytes = values[0] as long?;
-            return SizeSuffix(sizeInBytes ?? 0L, 2);
-        }
-
-
-        string SizeSuffix(long value, int decimalPlaces)
-        {
-            if (decimalPlaces < 0)
-                throw new ArgumentOutOfRangeException(nameof(decimalPlaces));
-            if (value < 0)
-                return "-" + SizeSuffix(-value, decimalPlaces);
-            if (value == 0)
-                return string.Format("{0:n" + decimalPlaces + "} bytes", 0);
-
-            // mag is 0 for bytes, 1 for KB, 2, for MB, etc.
-            int mag = (int)Math.Log(value, 1024);
-
-            // 1L << (mag * 10) == 2 ^ (10 * mag) 
-            // [i.e. the number of bytes in the unit corresponding to mag]
-            decimal adjustedSize = (decimal)value / (1L << (mag * 10));
-
-            // make adjustment when the value is large enough that
-            // it would round up to 1000 or more
-            if (Math.Round(adjustedSize, decimalPlaces) >= 1000)
-            {
-                mag += 1;
-                adjustedSize /= 1024;
-            }
-
-            return string.Format("{0:n" + decimalPlaces + "} {1}",
-                adjustedSize,
-                _sizeSuffixes[mag]);
+            return FileSystemObjectModel.SizeSuffix(sizeInBytes ?? 0L, 2);
         }
     }
 }
--- Jaya.Ui/Models/FileSystemObjectModel.cs
+++ Jaya.Ui/Models/FileSystemObjectModel.cs
@@ -4,6 +4,15 @@ namespace Jaya.Ui.Models
 {
     public abstract class FileSystemObjectModel : ModelBase
     {
+        static readonly string[] _sizeSuffixes;
+
+        static FileSystemObjectModel()
+        {
+            _sizeSuffixes = new string[] { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+        }
+
+        #region properties
+
         public string Id
         {
             get => Get<string>();
@@ -25,7 +34,22 @@ namespace Jaya.Ui.Models
         public long? Size
         {
             get => Get<long?>();
-            set => Set(value);
+            set
+            {
+                if (Set(value))
+                    RaisePropertyChanged(nameof(SizeString));
+            }
+        }
+
+        public string SizeString
+        {
+            get
+            {
+                if (Type == FileSystemObjectType.Directory)
+                    return string.Empty;
+
+                return SizeSuffix(Size ?? 0L, 2);
+            }
         }
 
         public FileSystemObjectType Type
@@ -58,9 +82,41 @@ namespace Jaya.Ui.Models
             set => Set(value);
         }
 
+        #endregion
+
         public override string ToString()
         {
             return Name;
+        }
+
+        // logic taken from https://stackoverflow.com/questions/14488796/does-net-provide-an-easy-way-convert-bytes-to-kb-mb-gb-etc
+        public static string SizeSuffix(long value, int decimalPlaces)
+        {
+            if (decimalPlaces < 0)
+                throw new ArgumentOutOfRangeException(nameof(decimalPlaces));
+            if (value < 0)
+                return "-" + SizeSuffix(-value, decimalPlaces);
+            if (value == 0)
+                return string.Format("{0:n" + decimalPlaces + "} bytes", 0);
+
+            // mag is 0 for bytes, 1 for KB, 2, for MB, etc.
+            int mag = (int)Math.Log(value, 1024);
+
+            // 1L << (mag * 10) == 2 ^ (10 * mag) 
+            // [i.e. the number of bytes in the unit corresponding to mag]
+            decimal adjustedSize = (decimal)value / (1L << (mag * 10));
+
+            // make adjustment when the value is large enough that
+            // it would round up to 1000 or more
+            if (Math.Round(adjustedSize, decimalPlaces) >= 1000)
+            {
+                mag += 1;
+                adjustedSize /= 1024;
+            }
+
+            return string.Format("{0:n" + decimalPlaces + "} {1}",
+                adjustedSize,
+                _sizeSuffixes[mag]);
         }
     }
 }
