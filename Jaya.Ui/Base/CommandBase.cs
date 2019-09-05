edd21b8de64b3c2f4d4c0208ea48027e6e179cc9
Rubal Walia <walia.rubal@gmail.com>
2019-09-05 17:00:23 +0530

    Added command base.

---
+++ Jaya.Ui/Base/CommandBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Jaya.Ui.Base
+{
+    public abstract class CommandBase : ModelBase, ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        #region properties
+
+        public bool IsEnabled
+        {
+            get => Get<bool>();
+            set
+            {
+                Set(value);
+                RaiseCanExecuteChanged();
+            }
+        }
+
+        public bool IsExecuting
+        {
+            get => Get<bool>();
+            protected set
+            {
+                Set(value);
+                RaiseCanExecuteChanged();
+            }
+        }
+
+        #endregion
+
+        private void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+                handler.Invoke(this, EventArgs.Empty);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return IsEnabled && !IsExecuting;
+        }
+
+        public abstract void Execute(object parameter);
+    }
+}
---
+++ Jaya.Ui/RelayCommand.cs
@@ -0,0 +1,25 @@
+﻿using Jaya.Ui.Base;
+using System;
+
+namespace Jaya.Ui
+{
+    public class RelayCommand<T> : CommandBase where T : class
+    {
+        readonly Action<T> _action;
+
+        public RelayCommand(Action<T> action)
+        {
+            _action = action;
+        }
+
+        public override void Execute(object parameter)
+        {
+            if (!CanExecute(parameter))
+                return;
+
+            IsExecuting = !IsExecuting;
+            _action.Invoke(parameter as T);
+            IsExecuting = !IsExecuting;
+        }
+    }
+}
