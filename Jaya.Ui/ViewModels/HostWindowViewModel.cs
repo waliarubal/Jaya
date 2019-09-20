1ad6aba9a3f7de8e721d9b2d0702e5d7b15335be
Rubal Walia <walia.rubal@gmail.com>
2019-09-20 18:21:52 +0530

    Staging

---
+++ Jaya.Ui/ViewModels/HostWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Jaya.Ui.Base;
+
+namespace Jaya.Ui.ViewModels
+{
+    public class HostWindowViewModel: ViewModelBase
+    {
+        public string Header
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+
+        public object Child
+        {
+            get => Get<object>();
+            set => Set(value);
+        }
+    }
+}
---
+++ Jaya.Ui/Views/HostWindowView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Jaya.Ui.Views
+{
+    public class HostWindowView : Window
+    {
+        public HostWindowView()
+        {
+            this.InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
