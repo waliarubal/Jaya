cb198fffe4eaeea85bbd0a414c0641af7a681d05
Rubal Walia <walia.rubal@gmail.com>
2019-10-04 19:00:15 +0530

    Staging

---
+++ Jaya.Ui/StyledWindow.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace Jaya.Ui
+{
+    public class StyledWindow: Window
+    {
+        protected override void OnTemplateApplied(TemplateAppliedEventArgs e)
+        {
+            base.OnTemplateApplied(e);
+        }
+    }
+}
