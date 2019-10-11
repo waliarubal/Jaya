61b91c697d7057a0bb24153d0856a0211f486654
Rubal Walia <walia.rubal@gmail.com>
2019-10-11 23:33:38 +0530

    Staging

---
+++ Jaya.Ui/Converters/IconImageConverter.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using Avalonia.Media.Imaging;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace Jaya.Ui.Converters
+{
+    public class IconImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is WindowIcon icon))
+                return null;
+
+            using (var stream = new MemoryStream())
+            {
+                icon.Save(stream);
+                stream.Seek(0, SeekOrigin.Begin);
+                return new Bitmap(stream);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
