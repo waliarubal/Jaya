cb290fcd3e45d883dd7120887db9c0ece1943363
Rubal Walia <walia.rubal@gmail.com>
2019-10-18 11:33:14 +0530

    Namespace fixes.

--- Jaya.Shared/Properties/AssemblyInfo.cs
+++ Jaya.Shared/Properties/AssemblyInfo.cs
@@ -1,8 +1,5 @@
-﻿using Portable.Xaml.Markup;
+﻿using Avalonia.Metadata;
 
-[assembly: XmlnsPrefix("https://github.com/waliarubal/Jaya", "j")]
 [assembly: XmlnsDefinition("https://github.com/waliarubal/Jaya", "Jaya.Shared")]
-[assembly: XmlnsDefinition("https://github.com/waliarubal/Jaya", "Jaya.Shared.Base")]
 [assembly: XmlnsDefinition("https://github.com/waliarubal/Jaya", "Jaya.Shared.Converters")]
 [assembly: XmlnsDefinition("https://github.com/waliarubal/Jaya", "Jaya.Shared.Models")]
-[assembly: XmlnsDefinition("https://github.com/waliarubal/Jaya", "Jaya.Shared.Services")]
