b47f30b1f15d8f72cc50befb4387f81a047c2669
Rubal Walia <walia.rubal@gmail.com>
2019-11-22 18:59:38 +0530

    Staging

--- Tools/deploy.bat
+++ Tools/deploy.bat
@@ -1,6 +1,6 @@
 @ECHO off
 
-SET 7Z=%APPVEYOR_BUILD_FOLDER%/Tools/7za.exe
+SET SEVEN_ZIP=%APPVEYOR_BUILD_FOLDER%/Tools/7za.exe
 
 ECHO Switch to build directory   
 CD %APPVEYOR_BUILD_FOLDER%
@@ -11,7 +11,7 @@ SET OUTPUT=%APPVEYOR_BUILD_FOLDER%/windows_%BUILD_VERSION%.zip
 SET OUTPUT_DIR=%APPVEYOR_BUILD_FOLDER%/windows
 SET PARAM=a -tzip %OUTPUT% %OUTPUT_DIR%/*
 CALL dotnet publish -c Release -r win-x64 --self-contained true --output %OUTPUT_DIR%   
-START /wait /B "" %7Z% %PARAM%
+START /wait /B "" %SEVEN_ZIP% %PARAM%
 
 ECHO;   
 ECHO Build for Linux (64-bit)   
@@ -19,7 +19,7 @@ SET OUTPUT=%APPVEYOR_BUILD_FOLDER%/linux_%BUILD_VERSION%.zip
 SET OUTPUT_DIR=%APPVEYOR_BUILD_FOLDER%/linux
 SET PARAM=a -tzip %OUTPUT% %OUTPUT_DIR%/*
 CALL dotnet publish -c Release -r linux-x64 --self-contained true --output %OUTPUT_DIR%
-START /wait /B "" %7Z% %PARAM%
+START /wait /B "" %SEVEN_ZIP% %PARAM%
 
 ECHO;   
 ECHO Build for Mac OS (64-bit)    
@@ -27,4 +27,4 @@ SET OUTPUT=%APPVEYOR_BUILD_FOLDER%/osx_%BUILD_VERSION%.zip
 SET OUTPUT_DIR=%APPVEYOR_BUILD_FOLDER%/osx
 SET PARAM=a -tzip %OUTPUT% %OUTPUT_DIR%/*
 CALL dotnet publish -c Release -r osx-x64 --self-contained true --output %OUTPUT_DIR%    
-START /wait /B "" %7Z% %PARAM%
+START /wait /B "" %SEVEN_ZIP% %PARAM%
