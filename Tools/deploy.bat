f701ee528a7fe32574b38e7d787b42016d211ec0
Rubal Walia <walia.rubal@gmail.com>
2019-11-22 19:41:33 +0530

    Staging

--- Tools/deploy.bat
+++ Tools/deploy.bat
@@ -30,6 +30,26 @@ SET PARAM=a -tzip %OUTPUT% %OUTPUT_DIR%/*
 CALL dotnet publish -c Release -r osx-x64 --self-contained true --output %OUTPUT_DIR%    
 START /wait /B "" %SEVEN_ZIP% %PARAM%
 
-SET RELEASE={"tag_name": "v%BUILD_VERSION%", "target_commitish": "dev", "name": "v%BUILD_VERSION%", "body": "", "draft": false, "prerelease": true}
+ECHO;
+ECHO Create GitHub release
+SET RELEASE={'tag_name': 'v%BUILD_VERSION%', 'target_commitish': 'dev', 'name': 'v%BUILD_VERSION%', 'body': '', 'draft': false, 'prerelease': true}
 SET PARAM=--data "%RELEASE%" https://api.github.com/repos/waliarubal/jaya/releases?access_token=%ACCESS_TOKEN%
+START /wait /B "" %CURL% %PARAM%
+
+ECHO;
+ECHO Upload Windows binaries
+SET OUTPUT=%APPVEYOR_BUILD_FOLDER%/windows_%BUILD_VERSION%.zip    
+SET PARAM=--header 'Content-Type:application/octet-stream' --data-binary @%OUTPUT% https://uploads.github.com/repos/waliarubal/jaya/releases/v%BUILD_VERSION%/assets?name=windows.zip&access_token=%ACCESS_TOKEN%
+START /wait /B "" %CURL% %PARAM%
+
+ECHO;
+ECHO Upload Linux binaries
+SET OUTPUT=%APPVEYOR_BUILD_FOLDER%/linux_%BUILD_VERSION%.zip    
+SET PARAM=--header 'Content-Type:application/octet-stream' --data-binary @%OUTPUT% https://uploads.github.com/repos/waliarubal/jaya/releases/v%BUILD_VERSION%/assets?name=linux.zip&access_token=%ACCESS_TOKEN%
+START /wait /B "" %CURL% %PARAM%
+
+ECHO;
+ECHO Upload Mac OS binaries
+SET OUTPUT=%APPVEYOR_BUILD_FOLDER%/osx_%BUILD_VERSION%.zip    
+SET PARAM=--header 'Content-Type:application/octet-stream' --data-binary @%OUTPUT% https://uploads.github.com/repos/waliarubal/jaya/releases/v%BUILD_VERSION%/assets?name=osx.zip&access_token=%ACCESS_TOKEN%
 START /wait /B "" %CURL% %PARAM%
