1c85e5b1d4c8c0a9056f25c2c61bd69d65c8e825
Rubal Walia <walia.rubal@gmail.com>
2019-12-12 14:28:51 +0530

    Started adding implementation for Ribbon. #12

---
+++ Jaya.Ui/Views/RibbonView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Jaya.Ui.Views
+{
+    public class RibbonView : UserControl
+    {
+        public RibbonView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
