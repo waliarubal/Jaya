f89419ce59e2df2e4ea6c59211942127252f1dc9
Rubal Walia <walia.rubal@gmail.com>
2019-12-18 15:04:31 +0530

    Changes for #12.

--- Jaya.Shared/Ribbon.cs
+++ Jaya.Shared/Ribbon.cs
@@ -1,4 +1,6 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Styling;
 using System;
 
@@ -7,13 +9,56 @@ namespace Jaya.Shared
     /// <summary>
     /// Refer https://www.powerworld.com/WebHelp/Content/MainDocumentation_HTML/Ribbons.htm for details.
     /// </summary>
-    public class Ribbon: TabControl, IStyleable
+    public class Ribbon : TabControl, IStyleable
     {
+        public static StyledProperty<bool> IsExpandedProperty;
+        Button _toggleButton;
+        ISelectable _selectedTab;
+
         static Ribbon()
         {
+            IsExpandedProperty = AvaloniaProperty.Register<Ribbon, bool>(nameof(IsExpanded), true);
+        }
 
+        public bool IsExpanded
+        {
+            get => GetValue(IsExpandedProperty);
+            set => SetValue(IsExpandedProperty, value);
         }
 
         Type IStyleable.StyleKey => typeof(Ribbon);
+
+        protected override void OnTemplateApplied(TemplateAppliedEventArgs e)
+        {
+            base.OnTemplateApplied(e);
+
+            _toggleButton = GetControl<Button>(e, "PART_ToggleButton");
+            _toggleButton.Click += delegate
+            {
+                IsExpanded = !IsExpanded;
+
+                if (IsExpanded)
+                {
+                    if (_selectedTab != null)
+                    {
+                        _selectedTab.IsSelected = true;
+                        _selectedTab = null;
+                    }
+                }
+                else
+                {
+                    if (SelectedItem != null)
+                    {
+                        _selectedTab = SelectedItem as ISelectable;
+                        _selectedTab.IsSelected = false;
+                    }
+                }
+            };
+        }
+
+        T GetControl<T>(TemplateAppliedEventArgs e, string name) where T : Control
+        {
+            return e.NameScope.Find<T>(name);
+        }
     }
 }
