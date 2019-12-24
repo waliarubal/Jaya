9663bc22862d07f43e0164cfd1a0fd21ba44c079
Rubal Walia <walia.rubal@gmail.com>
2019-12-24 19:37:04 +0530

    Removed unused constants.

--- Jaya.Ui/Constants.cs
+++ Jaya.Ui/Constants.cs
@@ -5,17 +5,6 @@
         public const string IMAGES_PATH_FORMAT = "avares://Jaya.Ui/Assets/Images/{0}";
         public const string APP_NAME = "Jaya - Cross Plat";
 
-        public static readonly string
-            COLLAPSED_IMAGE_URL,
-            EXPANDED_IMAGE_URL;
-
-
-        static Constants()
-        {
-            COLLAPSED_IMAGE_URL = "Folder-16.png".GetImageUrl();
-            EXPANDED_IMAGE_URL = "Folder-Open-16.png".GetImageUrl();
-        }
-
         public static string GetImageUrl(this string fileName)
         {
             return string.Format(IMAGES_PATH_FORMAT, fileName);
