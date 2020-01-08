b1022bcdd73c33aa2353b6fcb9866b39623a23fe
Rubal Walia <walia.rubal@gmail.com>
2020-01-08 20:30:10 +0530

    Added inverse boolean converter. Hidden header content and separator for modal windows. Added about window.

---
+++ Jaya.Shared/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace Jaya.Shared.Converters
+{
+    public class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            if (value is bool && targetType == typeof(bool))
+                return !(bool)value;
+
+            throw new ArgumentException("Passed value is not boolean.", nameof(value));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)Convert(value, targetType, parameter, culture);
+        }
+    }
+}
