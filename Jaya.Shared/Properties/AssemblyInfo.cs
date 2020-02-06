acc5a2f2eaf23bee394c479cb19cc80235b5b23f
Rubal Walia <walia.rubal@gmail.com>
2020-02-06 14:39:44 +0530

    Updated build number generation sequence.

--- Jaya.Shared/Properties/AssemblyInfo.cs
+++ Jaya.Shared/Properties/AssemblyInfo.cs
@@ -3,5 +3,4 @@
 [assembly: XmlnsDefinition("https://github.com/waliarubal/Jaya", "Jaya.Shared")]
 [assembly: XmlnsDefinition("https://github.com/waliarubal/Jaya", "Jaya.Shared.Converters")]
 [assembly: XmlnsDefinition("https://github.com/waliarubal/Jaya", "Jaya.Shared.Models")]
-
 [assembly: XmlnsDefinition("https://github.com/avaloniaui", "Jaya.Shared.Controls")]
