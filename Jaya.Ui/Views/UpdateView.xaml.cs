8c6c39499873c03dd71b63490555610d9166f81e
Rubal Walia <walia.rubal@gmail.com>
2020-02-07 13:44:07 +0530

    Added RDP access creds.

---
+++ Jaya.Ui/Views/UpdateView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Jaya.Ui.Views
+{
+    public class UpdateView : UserControl
+    {
+        public UpdateView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
