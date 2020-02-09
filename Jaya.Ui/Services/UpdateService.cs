76df5511632ec03ec9a9d6c356ab3061162ffc4e
Rubal Walia <walia.rubal@gmail.com>
2020-02-09 17:42:25 +0530

    Added update screen. #21

---
+++ Jaya.Ui/Services/UpdateService.cs
@@ -0,0 +1,12 @@
+﻿using Jaya.Shared.Services;
+using System.Composition;
+
+namespace Jaya.Ui.Services
+{
+    [Shared]
+    [Export(nameof(UpdateService), typeof(IService))]
+    public sealed class UpdateService: IService
+    {
+
+    }
+}
