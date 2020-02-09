763c905a80908baf6e18f861aada09b9d262f461
Rubal Walia <walia.rubal@gmail.com>
2020-02-09 22:18:29 +0530

    Staging changes related to update. #21

--- Jaya.Ui/Services/UpdateService.cs
+++ Jaya.Ui/Services/UpdateService.cs
@@ -1,6 +1,7 @@
 ﻿using Jaya.Shared.Services;
 using System;
 using System.Composition;
+using System.Reflection;
 
 namespace Jaya.Ui.Services
 {
@@ -8,10 +9,17 @@ namespace Jaya.Ui.Services
     [Export(nameof(UpdateService), typeof(IService))]
     public sealed class UpdateService: IService
     {
-        public Version Version => Constants.VERSION;
+        public UpdateService()
+        {
+            Version = Assembly.GetExecutingAssembly().GetName().Version;
+            VersionString = string.Format("{0}.{1}.{2}.{3}", Version.Major, Version.Minor, Version.Build, Version.Revision);
+            Bitness = Environment.Is64BitOperatingSystem ? (byte)64 : (byte)32;
+        }
 
-        public string VersionString => string.Format("{0}.{1}.{2}.{3}", Version.Major, Version.Minor, Version.Build, Version.Revision);
+        public Version Version { get; }
 
-        public byte Bitness => Environment.Is64BitOperatingSystem ? (byte)64 : (byte)32;
+        public string VersionString { get; }
+
+        public byte Bitness { get; }
     }
 }
