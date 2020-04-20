c630ca0c98242e5ed472b5131f701a07d37772dd
Rubal Walia <walia.rubal@gmail.com>
2020-04-20 14:08:18 +0530

    Fixed deployment artifact path for signing.

---
+++ build/sign.ps1
@@ -0,0 +1,8 @@
+Submit-SigningRequest `
+  -InputArtifactPath "publish/windows.exe" `
+  -CIUserToken "AEVquz4p/MFkP7KiMAEJMipcmYZZzXuavTXDTbZW6wPd" `
+  -OrganizationId "ec7e0b29-bda8-4762-8956-49e0648546a2" `
+  -ProjectKey "Jaya" `
+  -SigningPolicyKey "test-signing" `
+  -OutputArtifactPath "publish_signed/windows.exe" `
+  -WaitForCompletion
