4ccc0a5fd4da51482023dd62c7a8f4ebfe6e6d24
Rubal Walia <walia.rubal@gmail.com>
2020-06-02 13:29:27 +0530

    Added loading of all VMs in DI container. #73 Fixed amazon S3 provider not showing.

--- src/Jaya.Provider.S3/Services/S3Service.cs
+++ src/Jaya.Provider.S3/Services/S3Service.cs
@@ -3,29 +3,23 @@ using Jaya.Provider.S3.Models;
 using Jaya.Provider.S3.Views;
 using Jaya.Shared.Base;
 using Jaya.Shared.Models;
-using Jaya.Shared.Services;
 using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using Amazon.S3;
-using Amazon.S3.Model;
 
 namespace Jaya.Provider.S3.Services
 {
     public class S3Service : ProviderServiceBase
     {
-        private static IAmazonS3 client;
-        ConfigModel _config = new ConfigModel();
-        /// <summary>
-        /// Refer pages https://www.daimto.com/google-drive-authentication-c/ and https://www.daimto.com/google-drive-api-c/ for examples.
-        /// </summary>
+        static readonly IAmazonS3 client;
+        readonly ConfigModel _config = new ConfigModel();
+        
         public S3Service()
         {
             Name = "Amazon S3";
-            ImagePath = "avares://Jaya.Provider.GoogleDrive/Assets/Images/AmazonS3-32.png";
-            Description = "View your S3 account accounts, inspect their contents and play with directories & files stored within them.";
+            ImagePath = "avares://Jaya.Provider.S3/Assets/Images/AmazonS3-32.png";
+            Description = "View your Amazon S3 accounts, inspect their contents and play with directories & files stored within them.";
             ConfigurationEditorType = typeof(ConfigurationView);
         }
 
