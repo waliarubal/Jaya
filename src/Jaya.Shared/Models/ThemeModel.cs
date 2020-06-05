7c3d57bb31e6c89071c480269b0845e54e7a8088
Rubal Walia <walia.rubal@gmail.com>
2020-06-05 23:50:23 +0530

    Added theme manager for styling. #75

---
+++ src/Jaya.Shared/Models/ThemeModel.cs
@@ -0,0 +1,36 @@
+﻿using Avalonia.Markup.Xaml.Styling;
+using Avalonia.Styling;
+using Jaya.Shared.Base;
+using System;
+
+namespace Jaya.Shared.Models
+{
+    public class ThemeModel: ModelBase
+    {
+        public ThemeModel(string name, Uri themeUri)
+        {
+            Name = name;
+            Style = new StyleInclude(themeUri)
+            {
+                Source = themeUri
+            };
+        }
+
+        public string Name
+        {
+            get => Get<string>();
+            private set => Set(value);
+        }
+
+        public IStyle Style
+        {
+            get => Get<IStyle>();
+            private set => Set(value);
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ src/Jaya.Shared/ThemeManager.cs
@@ -0,0 +1,80 @@
+﻿using Avalonia.Controls;
+using Jaya.Shared.Base;
+using Jaya.Shared.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Jaya.Shared
+{
+    public sealed class ThemeManager: ModelBase
+    {
+        static readonly object _syncLock;
+        static ThemeManager _instance;
+        readonly List<ThemeModel> _themes;
+        readonly List<Window> _windows;
+
+        static ThemeManager()
+        {
+            _syncLock = new object();
+        }
+
+        private ThemeManager()
+        {
+            _themes = new List<ThemeModel>
+            {
+                new ThemeModel("Light", new Uri("avares://Jaya.Shared/Styles/Colors/BaseLight.xaml")),
+                new ThemeModel("Dark", new Uri("avares://Jaya.Shared/Styles/Colors/BaseDark.xaml"))
+            };
+
+            _windows = new List<Window>();
+        }
+
+        public static ThemeManager Instance
+        {
+            get
+            {
+                lock (_syncLock)
+                {
+                    if (_instance == null)
+                        _instance = new ThemeManager();
+
+                    return _instance;
+                }
+            }
+        }
+
+        public IEnumerable<ThemeModel> Themes => _themes;
+
+        public ThemeModel SelectedTheme
+        {
+            get => Get<ThemeModel>();
+            set
+            {
+                if (Design.IsDesignMode)
+                    return;
+
+                if (!Set(value) || value.Style == null)
+                    return;
+
+                foreach (var window in _windows)
+                    window.Styles[0] = SelectedTheme.Style;
+            }
+        }
+
+        internal void EnableTheme(Window window)
+        {
+            window.Opened += (sender, e) =>
+            {
+                _windows.Add(window);
+
+                if (SelectedTheme != null && SelectedTheme.Style != null)
+                    window.Styles[0] = SelectedTheme.Style;
+            };
+
+            window.Closing += (sender, e) =>
+            {
+                _windows.Remove(window);
+            };
+        }
+    }
+}
